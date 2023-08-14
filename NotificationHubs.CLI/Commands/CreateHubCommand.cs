119129d19f71d5a8f92da00688722cfe11c13a59
Tomek Kmiecik <kmiecik.t@gmail.com>
2023-08-14 17:30:04 +0200

    Adding CreateHubCommand

---
+++ NotificationHubs.CLI/Commands/CreateHubCommand.cs
@@ -0,0 +1,28 @@
+﻿using CommandLine;
+using Microsoft.Azure.NotificationHubs;
+using System;
+using System.Threading.Tasks;
+
+namespace NotificationHubs.Cli.Commands
+{
+    [Verb("create-hub", HelpText = "Creates new Notification Hub")]
+    public record CreateHubCommand : NamespaceLevelCommandBase
+    {
+        [Option("hub", Required = true)]
+        public string Hub { get; set; }
+
+        [Option("registration-ttl", Required = false)]
+        public TimeSpan? RegistrationTtl { get; set; }
+
+        protected override async Task<int> ExecuteAsync(NamespaceManager nsManager)
+        {
+            var hubDescription = new NotificationHubDescription(Hub);
+            hubDescription.RegistrationTtl = RegistrationTtl;
+
+            var result = await nsManager.CreateNotificationHubAsync(new NotificationHubDescription(Hub));
+            WriteCommandResult(result);
+
+            return 0;
+        }
+    }
+}
